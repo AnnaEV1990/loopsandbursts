8b243f34f2f7b28166e255d41d6b65e643252d33
Anna Evseykina <anytta9032@yandex.ru>
2024-02-10 19:21:44 +0700

    Initial commit

---
+++ src/test/java/ru/netology/loopsandburstsTest/services/MonthServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.loopsandburstsTest.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MonthServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/data.csv")
+
+    public void calculationOfTheNumberOfMonthsPerYear(int expected, int income, int expenses) {
+        MonthsService service = new MonthsService();
+
+        int actual = service.calculate(income, expenses);
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+}
